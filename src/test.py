cb4545ae0a1d444fa3436fa945d247c2fea7b3b5
Wilfried Huss <wilfried.huss@bartenbach.com>
2019-11-21 11:36:44 -0800

    add code to output the loaded graph

--- src/test.py
+++ src/test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import numpy as np
 from matplotlib import pyplot as plt
 from PIL import Image
@@ -9,14 +10,27 @@ fn = "./test_imgs/left_eye.png"
 im = Image.open(fn)
 eye = np.asanyarray(im)
 eye_scaled = eye[::3,::3] / 255.0
-input_frame = eye_scaled[np.newaxis, np.newaxis, ...]
+input_frame = eye_scaled[np.newaxis, ...]
 input_frame = input_frame.astype(np.float32)
+input_batch = np.stack([input_frame,input_frame], axis=0)
 #input_tensor = tf.convert_to_tensor(input_frame, dtype=tf.float32)
-input = {'eye': input_frame}
+input = {'eye': input_batch}
 
 with tf.compat.v1.Session() as session:
     model = ELG(session)
     output = model.inference(input)
 
+    with open("gazeml_standalone_network.txt", "w") as f:
+        graph = tf.get_default_graph()
+        operations = graph.get_operations()
+        for op in operations:
+            name = op.name
+            inputs = op.inputs
+            outputs = op.outputs
+            print(f"name:    {name}", file=f)
+            print(f"outputs: {outputs}", file=f)
+            print(40*"-", file=f)
+    import sys
+    sys.exit(0)
     from IPython import embed
     embed()
